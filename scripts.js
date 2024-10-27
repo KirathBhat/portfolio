81e85e0876fbcb0b57d5694f54023796b2ba0d15
Kirath Bhat <kirathuv2009@gmail.com>
2024-10-27 14:33:01 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,8 +5,29 @@ let index = 0;
 const professionSpan = document.getElementById("profession");
 
 function changeProfession() {
-    index = (index + 1) % professions.length;
-    professionSpan.textContent = professions[index];
+  index = (index + 1) % professions.length;
+  professionSpan.textContent = professions[index];
 }
+// Toggle the navigation menu
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  const navLinks = document.querySelector(".nav-links");
+  navLinks.classList.toggle("active");
+});
 
-setInterval(changeProfession, 3000);
+const navLinks = document.querySelectorAll(".nav-links .nav-item a");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    const navLinksContainer = document.querySelector(".nav-links");
+
+    // Remove 'active' class to hide the menu
+    navLinksContainer.classList.remove("active");
+
+    // Smooth scroll to target section
+    const targetId = this.getAttribute("href");
+    const targetSection = document.querySelector(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
